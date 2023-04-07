5ff243e4afea78adc7d1475d92e2ac46871a14d8
Christophe Combelles <ccomb@free.fr>
2023-04-07 21:54:36 +0200

    no key check

--- deploy.sh
+++ deploy.sh
@@ -11,4 +11,4 @@ else
 fi
 
 # define studio host in .ssh/config
-ssh studio "cd services/modelyz && $RESET && git pull && /opt/bin/docker-compose build && /opt/bin/docker-compose up -d"
+ssh modelyz "cd services/modelyz && $RESET && git pull && /opt/bin/docker-compose build && /opt/bin/docker-compose up -d"

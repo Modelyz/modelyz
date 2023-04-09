55d024fa19830ab1c199b2401cf33fbdf5af9539
Christophe Combelles <ccomb@free.fr>
2023-04-09 10:04:42 +0200

    fixed ws link

--- build.sh
+++ build.sh
@@ -4,9 +4,10 @@ if [ "$1" == "--local" ]; then
     WSS="ws://localhost:8080"
 elif [ "$1" == "--prod" ]; then
     DOMAIN=$2
-    WSS="ws://$DOMAIN:8080"
+    WSS="wss://$DOMAIN"
 else
     echo "use --local or --prod <domain.tld>"
     exit 1
 fi
-docker build --build-arg WSS=$WSS -t rg.fr-par.scw.cloud/prelab/modelyz .
+docker build --target build --cache-from modelyz-build -t modelyz-build .
+docker build --cache-from modelyz-build --build-arg WSS=$WSS -t rg.fr-par.scw.cloud/prelab/modelyz .

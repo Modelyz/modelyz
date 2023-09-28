05a82112d4eb882686264d55c0559824789374a2
Christophe Combelles <ccomb@free.fr>
2023-09-28 23:54:49 +0200

    better local build

--- build.local.sh
+++ build.local.sh
@@ -14,15 +14,9 @@ mkdir -p $DATADIR
 
 [ -f ./dist-newstyle/cache/plan.json ] && which cabal-cache && cabal-cache sync-from-archive --threads 4 --archive-uri s3://cabal-store.prelab.fr --host-name-override=s3.fr-par.scw.cloud --host-port-override=443 --host-ssl-override=True --region fr-par
 
-if [ "$1" == "-o" ]; then
-    # TODO stripping does not work
-    cabal build all $OPT_OPTIMIZE
-    cabal install $OPT_OPTIMIZE $OPT_INSTALL
-    #cabal v1-install -O2 --enable-split-objs --enable-executable-static --enable-executable-stripping --bindir=$INSTALLDIR
-else
-    cabal build all $OPT_DEVEL
-    cabal install $OPT_DEVEL $OPT_INSTALL
-fi
+    cabal build message -O2 --ghc-options="-Wall" --enable-library-stripping
+    cabal build studio store ident dumb -O2 --ghc-options="-Wall" --enable-library-stripping --enable-executable-static
+    cabal install studio store ident dumb --installdir=build
 
 which cabal-cache && cabal-cache sync-to-archive --threads 4 --archive-uri s3://cabal-store.prelab.fr --host-name-override=s3.fr-par.scw.cloud --host-port-override=443 --host-ssl-override=True --region fr-par
 

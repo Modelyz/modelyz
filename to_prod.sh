0a5ebf6d2912a42c61b4abfd8ce8511c3b4a11c0
Christophe Combelles <ccomb@free.fr>
2023-10-10 20:46:57 +0200

    adapted data deployment

--- to_prod.sh
+++ to_prod.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-scp data/eventstore.txt studio:/tmp/
-ssh studio docker cp /tmp/eventstore.txt modelyz-studio-1:/srv/data/eventstore.txt
-ssh studio docker restart modelyz-studio-1
+scp studio/data/messagestore.txt modelyz:/tmp/studio.messagestore.txt
+scp store/data/messagestore.txt modelyz:/tmp/store.messagestore.txt
+scp dumb/data/messagestore.txt modelyz:/tmp/dumb.messagestore.txt
+scp ident/data/messagestore.txt modelyz:/tmp/ident.messagestore.txt
+
+ssh modelyz docker cp /tmp/studio.messagestore.txt modelyz-studio-1:/srv/studio/data/messagestore.txt
+ssh modelyz docker cp /tmp/store.messagestore.txt modelyz-store-1:/srv/store/data/messagestore.txt
+ssh modelyz docker cp /tmp/dumb.messagestore.txt modelyz-dumb-1:/srv/dumb/data/messagestore.txt
+ssh modelyz docker cp /tmp/ident.messagestore.txt modelyz-ident-1:/srv/ident/data/messagestore.txt
+
+ssh modelyz docker restart modelyz-studio-1
+ssh modelyz docker restart modelyz-store-1
+ssh modelyz docker restart modelyz-dumb-1
+ssh modelyz docker restart modelyz-ident-1

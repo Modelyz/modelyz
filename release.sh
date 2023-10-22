877667276c951d6fed47f970c5e344f0482dd146
Christophe Combelles <ccomb@free.fr>
2023-10-22 11:05:23 +0200

    fixed release and dockerfile

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x
+set -xe
 
 MESSAGE=1
 STUDIO=17
@@ -7,14 +7,14 @@ STORE=1
 IDENT=1
 DUMB=1
 
-pushd ../message
+pushd message
 sed -i "s/version:            .*/version:            0.$MESSAGE.0.0/" modelyz-message.cabal
 ./build.sh
 git diff-index --quiet HEAD -- || git ci -a -m "new version $MESSAGE"
 git tag | grep ^$MESSAGE$ || git tag $MESSAGE
 popd
 
-pushd ../studio
+pushd studio
 sed -i "s/export APPVERSION=.*/export APPVERSION=$STUDIO/" front/build.sh
 sed -i "s/version:            .*/version:            0.$STUDIO.0.0/" back/modelyz-studio.cabal
 ./build.sh
@@ -22,21 +22,21 @@ git diff-index --quiet HEAD -- || git ci -a -m "new version $STUDIO"
 git tag | grep ^$STUDIO$ || git tag $STUDIO
 popd
 
-pushd ../store
+pushd store
 sed -i "s/version:            .*/version:            0.$STORE.0.0/" modelyz-store.cabal
 ./build.sh
 git diff-index --quiet HEAD -- || git ci -a -m "new version $STORE"
 git tag | grep ^$STORE$ || git tag $STORE
 popd
 
-pushd ../ident
+pushd ident
 sed -i "s/version:            .*/version:            0.$IDENT.0.0/" modelyz-ident.cabal
 ./build.sh
 git diff-index --quiet HEAD -- || git ci -a -m "new version $IDENT"
 git tag | grep ^$IDENT$ || git tag $IDENT
 popd
 
-pushd ../dumb
+pushd dumb
 sed -i "s/version:            .*/version:            0.$DUMB.0.0/" modelyz-dumb.cabal
 ./build.sh
 git diff-index --quiet HEAD -- || git ci -a -m "new version $DUMB"
